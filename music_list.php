d70ed56c10d5ca5b3e2302cd41ce186607cd3496
SgnvJaber <62845454+SgnvJaber@users.noreply.github.com>
2020-04-08 12:35:51 +0300

    Delete music_list.php

--- music_list.php
+++
@@ -1,7 +0,0 @@
-<?php>$data=[
-    ["artist_name"=> "Hayashi Yuki","name"=>"The Spear and Shield Match", "id"=>"KNVJLJX4JV4"],
-    ["artist_name"=> "Hayashi Yuki","name"=>"I'll Be Your Hero", "id"=>"5ML83Mo0mAc"],
-    ["artist_name"=> "Go Shiina","name"=>"Thunder Breathing", "id"=>"RuGBkr2eaPk"],
-    ];
-header('Content-Type: application/json');
-echo json_encode($data);
